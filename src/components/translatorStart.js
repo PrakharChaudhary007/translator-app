39359f76f5b2f571f3bc1c41ac4dc18356b09d3d
PrakharChaudhary007 <8840518862sanket@gmail.com>
2025-12-16 00:10:51 +0530

    Update greeting message in TranslatorStart component
    oahili baar change kiya hai aacha lag rha hai

--- src/components/translatorStart.js
+++ src/components/translatorStart.js
@@ -1,6 +1,6 @@
 function TranslatorStart({onStart}){
     return <div className="flex flex-col p-4">
-        Hi I am translator Start
+        Hi I am translator Start And am editing the code in the git hub 
     <button className="">
 
     </button>
